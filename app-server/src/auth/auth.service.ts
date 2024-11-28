e7897d5cf1233aa0b71adf45535df52ce2e49fbc
levvysokiy <lev@lexense.com>
2024-11-28 22:02:58 +0200

    dockerize everything

--- app-server/src/auth/auth.service.ts
+++ app-server/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { OwnersService } from '../owners/owners.service';
 import { RegisterRequestDto } from './dto/register-request.dto';
 import { Owner } from '../owners/owners.shema';
